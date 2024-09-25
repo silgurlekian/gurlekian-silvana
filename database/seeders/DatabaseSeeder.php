31c71276c863e936e38c1fb12309172b287ac0f4
silgurlekian <sgurlekian@gmail.com>
2024-09-25 11:50:28 -0300

    agregar usuario admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Silvana Gurlekian',
+            'email' => 'silvana.gurlekian@davinci.edu.ar',
+            'password' => 'admin123',
         ]);
 
         $this->call([
