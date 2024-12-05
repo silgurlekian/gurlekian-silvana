2fb5c02bf83ee56a22dcaf4d99a263770cbc2fe5
Silvana <silvana.gurlekian@davinci.edu.ar>
2024-12-05 15:17:43 -0300

    cambia bcrypt por hash para contraseñas

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash; 
 use App\Models\User;
 
 class AuthController extends Controller
@@ -34,7 +35,8 @@ class AuthController extends Controller
         User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' => bcrypt($request->password),
+            // Usar Hash para hashear la contraseña
+            'password' => Hash::make($request->password),
             'role' => 'user', // Asigna el rol predeterminado
         ]);
 
